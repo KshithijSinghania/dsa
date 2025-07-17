7278d697b1942e18d4e72b9f083278c75658c52f
KshithijSinghania <142089913+KshithijSinghania@users.noreply.github.com>
2025-07-18 01:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (70.09%) - LeetSync

---
+++ 2849-sum-of-imbalance-numbers-of-all-subarrays/sum-of-imbalance-numbers-of-all-subarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumImbalanceNumbers(vector<int>& A) {
+        int res = 0, n = A.size();
+        vector<int> left(n), seen(n + 2, -1);
+        for (int i = 0; i < n; i++) {
+            left[i] = max(seen[A[i] + 1], seen[A[i]]);
+            seen[A[i]] = i;
+        }
+        seen.assign(n + 2, n);
+        for (int i = n - 1; i >= 0; i--) {
+            seen[A[i]] = i;
+            res += (i - left[i]) * (seen[A[i] + 1] - i);
+        }
+        return res - n * (n + 1) / 2;
+    }
+};
