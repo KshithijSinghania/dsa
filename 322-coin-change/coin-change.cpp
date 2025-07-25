ac3e82cf0c9ab07987828f8262938c1beeae85e6
KshithijSinghania <142089913+KshithijSinghania@users.noreply.github.com>
2025-07-25 23:07:40 +0530

    Time: 52 ms (21.99%) | Memory: 49.1 MB (35.73%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -20,8 +20,25 @@ vector <vector <int>> dp;
         int n=coins.size();
         vector <int> temp(amount+1,-1);
         dp.resize(n,temp);
-        int val=solve(n-1,amount,coins);
-        
+        // int val=solve(n-1,amount,coins);
+        int tot=amount;
+        for(int idx=0;idx<n;idx++){
+            for(int amount=0;amount<=tot;amount++){
+                if(idx==0){
+                   if(amount%coins[idx]==0)
+                    dp[idx][amount]=amount/coins[idx];
+                    else
+                    dp[idx][amount]=1e9;
+                    continue;
+                }
+                int take=1e9;
+                if(coins[idx]<=amount)
+                take=1+dp[idx][amount-coins[idx]];
+                int nottake=dp[idx-1][amount];
+                dp[idx][amount]=min(take,nottake);
+            }
+        }
+        int val=dp[n-1][tot];
         return val>=1e9?-1:val;
     }
 };
