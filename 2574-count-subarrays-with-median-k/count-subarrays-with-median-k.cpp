2b448c298379688e2581d92e3c7e1bb95fb25ed0
KshithijSinghania <142089913+KshithijSinghania@users.noreply.github.com>
2026-09-13 23:42:07 +0530

    Time: 28 ms (47.49%) | Memory: 56.5 MB (55.25%) - LeetSync

--- 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.cpp
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     int countSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-
-        int kid = find(nums.begin(), nums.end(), k) - nums.begin();
-
-        unordered_map<int, int> mp;
-
-        int balance = 0;
-        mp[0]++;
-
-        // Process elements to the right of k
-        for (int i = kid + 1; i < n; i++) {
-            balance += (nums[i] > k ? 1 : -1);
-            mp[balance]++;
+        unordered_map <int,int> mp;
+        int kid=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==k){
+                kid=i;
+                break;
+            }
         }
-
-        int ans = mp[0] + mp[1];
-
-        balance = 0;
-
-        // Process elements to the left of k
-        for (int i = kid - 1; i >= 0; i--) {
-            balance += (nums[i] > k ? 1 : -1);
-
-            ans += mp[-balance];
-            ans += mp[1 - balance];
+        int d1=0;
+        mp[d1]++;
+        for(int i=kid+1;i<n;i++){
+            if(nums[i]>k) d1++;
+            else d1--;
+            mp[d1]++;
         }
-
+        int d2=0;
+        int ans=0;
+        ans+=(mp[1-d2]+mp[-1*d2]);
+        for(int i=kid-1;i>=0;i--){
+            if(nums[i]>k) d2++;
+            else d2--;
+            ans+=(mp[1-d2]+mp[-1*d2]);
+        }
         return ans;
     }
 };
